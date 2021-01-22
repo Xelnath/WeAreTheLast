3bce18edad43441b9139037394e975e2b09fc7f5
Alexander Brazie <alex@xelnath.com>
2021-01-22 10:19:52 -0800

    [MainMenu] Now tracking death count

--- code_unity/We Are The Last/Assets/Scripts/MMControl.cs
+++ code_unity/We Are The Last/Assets/Scripts/MMControl.cs
@@ -1,26 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MMControl : MonoBehaviour
 {
+    public TextMeshProUGUI AttemptsLabel;
     public string LevelScene;
+
+    [Serializable]
+    public class FailureLabel
+    {
+        public int MinFailures = 0;
+        public string Label = "Failures: {0}";
+    }
+
+    public FailureLabel[] Failures = new FailureLabel[0];
+    
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        int deathCount = PlayerPrefs.GetInt( "FAILURES", 0 );
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for ( int i = Failures.Length - 1; i >= 0; i-- )
+        {
+            if ( Failures[i].MinFailures < deathCount )
+            {
+                AttemptsLabel.text = string.Format( Failures[i].Label, deathCount );
+                break;
+            }
+        }
     }
 
     public void StartScene()
     {
         SceneManager.LoadScene(LevelScene);
+    }
+
+    public void ResetEscapes()
+    {
+        PlayerPrefs.SetInt( "FAILURES", 0 );
+        PlayerPrefs.Save();
     }
 
     public void Quit()
